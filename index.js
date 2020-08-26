ac6308a8cf461bfb043847f444f3e557be927547
cseibert2667 <cseibert2667@gmail.com>
2020-08-25 20:29:42 -0700

    all questions written out

--- index.js
+++ index.js
@@ -1,6 +1,53 @@
+const inquire = require("inquirer")
+const fs = require("fs")
+
 // array of questions for user
 const questions = [
-
+    {
+        type: "input",
+        message: "Please enter your GitHub username:",
+        name: "username"
+    },
+    {
+        type: "input",
+        message: "Please enter your email address:",
+        name: "email"
+    },
+    {
+        type: "input",
+        message: "Please enter the name of your project:",
+        name: "title"
+    },
+    {
+        type: "input",
+        message: "Please enter a brief description of your project:",
+        name: "description"
+    },
+    {
+        type: "input",
+        message: "What kind of license should your project have?",
+        name: "license"
+    },
+    {
+        type: "input",
+        message: "What command should be run to install dependencies?",
+        name: "installation"
+    },
+    {
+        type: "input",
+        message: "What command should be run to run tests?",
+        name: "tests"
+    },
+    {
+        type: "input",
+        message: "What should the user know about using the repo?",
+        name: "usage"
+    },
+    {
+        type: "input",
+        message: "What should the user know about contributing to the repo?",
+        name: "contributing"
+    },
 ];
 
 // function to write README file
