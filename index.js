f1649a140849dae204e530b07e2911da13e40d28
cseibert2667 <cseibert2667@gmail.com>
2020-08-26 14:57:29 -0700

    logic is built

--- index.js
+++ index.js
@@ -1,50 +1,42 @@
 const inquire = require("inquirer")
 const fs = require("fs")
+const markdown = require("./utils/generateMarkdown")
 
 // array of questions for user
 const questions = [
     {
-        type: "input",
         message: "Please enter your GitHub username:",
         name: "username"
     },
     {
-        type: "input",
         message: "Please enter your email address:",
         name: "email"
     },
     {
-        type: "input",
         message: "Please enter the name of your project:",
         name: "title"
     },
     {
-        type: "input",
         message: "Please enter a brief description of your project:",
         name: "description"
     },
     {
-        type: "input",
         message: "What kind of license should your project have?",
         name: "license"
     },
     {
-        type: "input",
         message: "What command should be run to install dependencies?",
         name: "installation"
     },
     {
-        type: "input",
         message: "What command should be run to run tests?",
         name: "tests"
     },
     {
-        type: "input",
         message: "What should the user know about using the repo?",
         name: "usage"
     },
     {
-        type: "input",
         message: "What should the user know about contributing to the repo?",
         name: "contributing"
     },
@@ -52,39 +44,19 @@ const questions = [
 
 // function to write README file
 function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, function(err){
+        console.log("file written")
+    })
 }
 
 // function to initialize program
 function init() {
     inquire.prompt(questions)
-    .then(function(answers){
-        console.log(answers)
+    .then(function(data){
+        const readme = markdown(data);
+        writeToFile(`${data.title}-README.md`, readme)
     })
 }
 
 // function call to initialize program
 init();
-
-
-// FORMAT OF README (markdown included)
-// ![license badge](https://img.shields.io/badge/license-<USER INPUT>-blue)
-// # PROJECT TITLE
-// ## DESCRIPTION
-    // description input goes here
-// ## TABLE OF CONTENTS
-    // * [item one](#item one)
-    // * [item two](#item two)
-    // * [item three](#item three)
-    // * [item four](#item four)
-// ## INSTALLATION
-    // installation input goes here
-// ## USAGE
-    // usage input goes here
-// ## LICENSE
-    // "This project is licensed under the [USER INPUT] license"
-// ## CONTRIBUTING
-    // contributing input goes here
-// ## TESTING
-    // to run tests, please run `USER INPUT` in the command line
-// ## QUESTIONS
-    // contact at [email input], check out more of my GitHub projects at [username input]
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,29 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-
+  return `
+  ![license badge](https://img.shields.io/badge/license-${data.license}-blue)
+  # ${data.title}
+  ## DESCRIPTION
+  ${data.description}
+  ## TABLE OF CONTENTS
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Testing](#testing)
+  * [Questions](#questions)
+  ## INSTALLATION
+  Please run \`${data.installation}\` in the command line to install dependencies.
+  ## USAGE
+  ${data.usage}
+  ## LICENSE
+  This project is licensed under the ${data.license} license.
+  ## CONTRIBUTING
+  ${data.contributing}
+  ## TESTING
+  To run tests, please run \`${data.tests}\` in the command line
+  ## QUESTIONS
+  All inquiries can be sent to ${data.email}, and be sure to check out more of my GitHub projects at [${data.username}](https://www.github.com/${data.username})!
 `;
 }
 
