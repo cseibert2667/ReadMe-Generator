b51e7cd4bff85bade94c1a45c22c3348f759711e
cseibert2667 <cseibert2667@gmail.com>
2020-08-27 15:17:32 -0700

    moved questions array to questions.js

--- index.js
+++ index.js
@@ -1,46 +1,7 @@
 const inquire = require("inquirer")
 const fs = require("fs")
 const markdown = require("./utils/generateMarkdown")
-
-// array of questions for user
-const questions = [
-    {
-        message: "Please enter your GitHub username:",
-        name: "username"
-    },
-    {
-        message: "Please enter your email address:",
-        name: "email"
-    },
-    {
-        message: "Please enter the name of your project:",
-        name: "title"
-    },
-    {
-        message: "Please enter a brief description of your project:",
-        name: "description"
-    },
-    {
-        message: "What kind of license should your project have?",
-        name: "license"
-    },
-    {
-        message: "What command should be run to install dependencies?",
-        name: "installation"
-    },
-    {
-        message: "What command should be run to run tests?",
-        name: "tests"
-    },
-    {
-        message: "What should the user know about using the repo?",
-        name: "usage"
-    },
-    {
-        message: "What should the user know about contributing to the repo?",
-        name: "contributing"
-    },
-];
+const questions = require("./utils/questions")
 
 // function to write README file
 function writeToFile(fileName, data) {
